63506f4c75101af12c30ce9a4bf3def05b291bc6
Michał Woś <mw792829570@gmail.com>
2015-11-01 10:41:19 +0100

    compiler main

--- src/CompilerJVM.hs
+++ src/CompilerJVM.hs
@@ -2,6 +2,8 @@ module Main where
 
 import System.IO ( stdin, stderr, hGetContents, hPutStrLn )
 import System.Environment ( getArgs, getProgName )
+import Data.List
+import System.Process
 
 import ErrM
 import LexInstant
@@ -20,6 +22,22 @@ putErr s = hPutStrLn stderr s
 runFile :: FilePath -> IO ()
 runFile path = readFile path >>= run path
 
+runLLVM :: Program -> String -> String -> IO ()
+runLLVM tree basedir name = do
+	out <- compileLLVM tree name
+	let newPath = (basedir ++ name ++ ".ll")
+	writeFile newPath out
+	_ <- runCommand $ "llvm-as " ++ newPath
+	return ()
+
+runJVM :: Program -> String -> String -> IO ()
+runJVM tree basedir name = do
+	out <- compileJVM tree name
+	let newPath = (basedir ++ name ++ ".j")
+	writeFile newPath out
+	_ <- runCommand $ unwords ["java -jar lib/jasmin.jar -d", basedir, newPath, ">> /dev/null"]
+	return ()
+
 run :: String -> String -> IO ()
 run path code = do
 	let ts = myLLexer code
@@ -32,8 +50,12 @@ run path code = do
 			isCorrect <- staticCheck tree
 			case isCorrect of
 				True -> do
-					out <- compileLLVM tree "nazwa" --todo nazwa
-					putStr out
+					let (Just i) = findIndex (\a -> a == '/') (reverse path)
+					let (basedir, file) = splitAt (length path - i) path
+					let (Just j) = findIndex (\a -> a == '.') (reverse file)
+					let name = take (length file - j - 1) file
+					runJVM tree basedir name
+					return ()
 				False -> return ()
 
 
