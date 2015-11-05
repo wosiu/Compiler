9124e57a2c0ae86740dd541c52fd68c8f8e17700
Michał Woś <mw792829570@gmail.com>
2015-11-05 11:52:05 +0100

    test script

---
+++ test.sh
@@ -0,0 +1,20 @@
+for f in `ls --format=single-column -1 tests/*.ins`
+do
+	pref=$(echo $f | cut -d'.' -f 1)
+	name=$(echo $pref | cut -d'/' -f 2)
+	eval=$pref."eval"
+	#echo "pref: "$pref
+	#echo "name: "$name
+	#echo "eval: "$eval
+	#./insc_jvm $f
+	#java -classpath tests/ $name > tmp_jvm.out
+	#diff -sq tmp_jvm.out $eval || break
+	./insc_llvm $f
+	lli $pref".bc" > tmp_llvm.out
+	diff -sq tmp_llvm.out $eval || break
+done
+
+cd tests
+#rm *.class *.jasmin *.ll *.bc
+cd ..
+
